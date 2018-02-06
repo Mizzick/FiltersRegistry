bf43ccc0fdce8d7e607f3d5ec6113361c42a9581
mizzick <ds.kolyshev@gmail.com>
2018-02-06 17:03:14 +0700

    travis build

---
+++ push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_files() {
+  git commit --message "[ci skip] Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add github https://${GITHUB_TOKEN}@github.com/Mizzick/FiltersRegistry.git > /dev/null 2>&1
+  git push --quiet --set-upstream github master
+}
+
+setup_git
+commit_files
+upload_files
+
