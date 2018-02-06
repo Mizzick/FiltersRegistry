0672b9ed8c95ca65a45389f14cfb25e1a9481ecd
mizzick <ds.kolyshev@gmail.com>
2018-02-06 18:23:00 +0700

    travis build

--- push.sh
+++ push.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-set -x
-
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_files() {
+  git checkout
   git add ./filters
   git commit --message "[ci skip] Travis build: $TRAVIS_BUILD_NUMBER"
 }
